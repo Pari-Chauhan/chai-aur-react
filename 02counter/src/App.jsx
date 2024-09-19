2334e260b5fa1bebc010018c49fe40b7af9258cb
Pari Chauhan <123296817+Pari-Chauhan@users.noreply.github.com>
2024-09-19 13:02:29 +0530

    Update App.jsx

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+//We made a function named addValue which updares the value of counter everytime button is clicked
+//When we checked, the updation was happening in the consolei.e. our JS output screen but it wasn't ahappening in the UI
+//To enable this change React uses hooks useState(makes the update in all the places of UI where we have the given variable)
 function App() {
 
-  const [counter, setCounter]  = useState(15)
+  const [counter, setCounter]  = useState(15) // -> userState takes default value
+  //returns an array of size 2, 0th contains variable and 1st is function
 
   //let counter = 15
 
   const addValue = () => {
-    //counter = counter + 1
+    //counter = counter + 1 OR setCounter(counter)
+    //setCounter(counter + 1)
     setCounter(prevCounter => prevCounter + 1)
     setCounter(prevCounter => prevCounter + 1 )
     setCounter(prevCounter => prevCounter + 1)
