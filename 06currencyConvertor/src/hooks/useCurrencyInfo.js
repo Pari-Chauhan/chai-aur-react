6719bdf5e2dc3510600283e9b22a4aa2ecca7ce7
Pari Chauhan <123296817+Pari-Chauhan@users.noreply.github.com>
2024-09-28 13:08:43 +0530

    Update useCurrencyInfo.js

--- 06currencyConvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,3 +1,9 @@
+
+// made it js not jsx as hooks contains mostly js code
+// fetch is used to call API and also have then chain(which takes the arrow function as argument)
+// The API is such that url k saath whatever currency is passed it'll give the list of multiple factor and name for each currency
+// used setData inside the useEffect function because whenever we fetch API we set data(or changes the state)
+// This is how a custom hook is created
 import {useEffect, useState} from "react"
 
 
